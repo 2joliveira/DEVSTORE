62d1961a9dee0af7f226432a7f661ab390c8232c
Jefferson Oliveira <oliveira2112jj@outlook.com>
2024-01-22 21:12:52 -0300

    loading search

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -41,7 +41,7 @@ export default async function Search({ searchParams }: SearchProps) {
         {products.map((product) => (
           <Link
             key={product.id}
-            href={`/prouct/${product.slug}`}
+            href={`/product/${product.slug}`}
             className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
           >
             <Image
