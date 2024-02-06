a04a69eb729c23cdab381399cb985e0a99fb1069
Jefferson Oliveira <oliveira2112jj@outlook.com>
2024-02-06 18:31:51 -0300

    adjusts to app deploy

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -2,6 +2,8 @@
 
 import { Skeleton } from "@/components/skeleton";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { CurrentSearch } from "./current-search";
 
 export default function SearchLoading() {
   const searchParams = useSearchParams();
@@ -9,9 +11,9 @@ export default function SearchLoading() {
 
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[400px]" />
